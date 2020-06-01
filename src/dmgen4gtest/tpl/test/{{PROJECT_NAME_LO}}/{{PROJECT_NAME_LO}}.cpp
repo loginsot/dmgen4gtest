e9a1a47698fdc2bdd5b6a7b0e885157dfe4aff17
brinkqiang <brink.qiang@gmail.com>
2020-06-01 11:35:37 +0800

    add

--- src/dmgen4gtest/tpl/test/{{PROJECT_NAME_LO}}/{{PROJECT_NAME_LO}}.cpp
+++ src/dmgen4gtest/tpl/test/{{PROJECT_NAME_LO}}/{{PROJECT_NAME_LO}}.cpp
@@ -1,34 +1,41 @@
 
 #include "gtest.h"
 
-class env_{{PROJECT_NAME}} : public testing::Test
+class env_{{PROJECT_NAME}}
+{
+public:
+    void init(){}
+    void uninit(){}
+};
+
+class frame_{{PROJECT_NAME}} : public testing::Test
 {
 public:
     virtual void SetUp()
     {
-
+        env.init();
     }
     virtual void TearDown()
     {
-
+        env.uninit();
     }
 protected:
+    env_{{PROJECT_NAME}} env;
+};
 
-}
-
-TEST_F(env_{{PROJECT_NAME}}, init)
+TEST_F(frame_{{PROJECT_NAME}}, init)
 {
     ASSERT_TRUE(1);
     ASSERT_EQ(1, 1);
     ASSERT_FALSE(0);
 }
 
-TEST_F(env_{{PROJECT_NAME}}, create)
+TEST_F(frame_{{PROJECT_NAME}}, create)
 {
 
 }
 
-TEST_F(env_{{PROJECT_NAME}}, do_action)
+TEST_F(frame_{{PROJECT_NAME}}, do_action)
 {
 
 }
