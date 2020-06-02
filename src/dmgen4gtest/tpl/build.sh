79770f966776fca2b38a615ca4a5d54a890f1520
brinkqiang <brink.qiang@gmail.com>
2020-06-02 21:36:14 +0800

    add

--- src/dmgen4gtest/tpl/build.sh
+++ src/dmgen4gtest/tpl/build.sh
@@ -14,11 +14,10 @@ rm -rf build
 mkdir -p build
 pushd build
 
-cmake -DCMAKE_BUILD_TYPE=relwithdebinfo ..
+cmake -DCMAKE_BUILD_TYPE=debug ..
 cmake --build . --config debug
 
 popd
-
 # popd
 
 # echo continue && read -n 1
